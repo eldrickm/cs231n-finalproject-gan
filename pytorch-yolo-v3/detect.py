37ec752e880988e3ee7c3071ae38b71a581533d2
Eldrick Millares <eldrick@stanford.edu>
2018-05-29 07:40:45 +0000

    Detect gives out bounding boxes

--- pytorch-yolo-v3/detect.py
+++ pytorch-yolo-v3/detect.py
@@ -270,20 +270,22 @@ if __name__ ==  '__main__':
     
     draw = time.time()
 
-
+    
     def write(x, batches, results):
         c1 = tuple(x[1:3].int())
         c2 = tuple(x[3:5].int())
         img = results[int(x[0])]
         cls = int(x[-1])
         label = "{0}".format(classes[cls])
-        color = random.choice(colors)
-        cv2.rectangle(img, c1, c2,color, 1)
-        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
-        c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
-        cv2.rectangle(img, c1, c2,color, -1)
-        cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1)
+        color = (0,0,0) #random.choice(colors)
+        if label == "person":
+          cv2.rectangle(img, c1, c2,color, -1)
+        #t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
+        #c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
+        #cv2.rectangle(img, c1, c2, color, -1)
+        #cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1)
         return img
+    
     
             
     list(map(lambda x: write(x, im_batches, orig_ims), output))
@@ -309,9 +311,3 @@ if __name__ ==  '__main__':
 
     
     torch.cuda.empty_cache()
-    
-    
-        
-        
-    
-    
